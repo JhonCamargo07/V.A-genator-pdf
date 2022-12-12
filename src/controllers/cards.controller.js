41a5993ba9b93a97ae87b08a509dbe73fc15b642
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2022-12-12 18:51:12 -0500

    Se genera un certificado personal, con datos quemados, ya se tiene la plantilla para las cartas

--- src/controllers/cards.controller.js
+++ src/controllers/cards.controller.js
@@ -1,10 +1,13 @@
 const pdfPrinter = require('pdfmake');
 const fs = require('fs');
 const PdfPrinter = require('pdfmake');
-const fonts = require('./../models/fonts');
 const { join } = require('path');
 const { v4: uuidv4 } = require('uuid');
 const cardsCtrl = {};
+
+const fonts = require('./../models/fonts');
+const styles = require('./../models/style');
+
 const printer = new PdfPrinter(fonts);
 
 cardsCtrl.laborCard = async (req, res) => {
@@ -24,9 +27,26 @@ cardsCtrl.familyCard = async (req, res) => {
 cardsCtrl.personalCard = async (req, res) => {
 	var contentCard = {
 		content: [
-			'First paragraph',
-			'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines',
+			{ text: 'CERTIFICACI\u00d3N PERSONAL', style: ['header', 'bold'] },
+			{
+				text: 'Por medio de la presente, yo AMPARO CARDENAS VILLAMIL identificada con c\u00e9dula de ciudadan\u00eda No. 36.542.666 de N\u00e1taga (Huila), certifico que conozco desde hace 18 a\u00f1os al se\u00f1or JHON ALEXANDER CAMARGO CADENA identificado con c\u00e9dula de ciudadan\u00eda N\u00b0 2.101.010.101 de Bogot\u00e1, es una persona honrada, trabajadora, perseverante y cumplidora de su deber.',
+				style: ['parrafo', 'superMargin'],
+			},
+			{
+				text: 'Por tal motivo doy fe.',
+				style: ['parrafo', 'marginTop'],
+			},
+			{
+				text: 'Se expide en Bogota, a los 12 dias del mes de diciembre de 2022.',
+				style: ['parrafo', 'marginTop'],
+			},
+			{ text: 'Att:', style: ['marginFirma', 'bold'] },
+			{ text: '_______________________', style: ['marginRight', 'bold', 'font13'] },
+			{ text: 'AMPARO CARDENAS VILLAMIL', style: ['marginRight', 'bold', 'font13'] },
+			{ text: 'C.C: N° 36.542.666 de Nátaga (Huila)', style: ['marginRight', 'bold', 'font13'] },
+			{ text: 'CEL: 315 762 1254', style: ['marginRight', 'bold', 'font13'] },
 		],
+		styles: styles,
 	};
 
 	const nameFile = uuidv4();
---
+++ src/models/style.js
@@ -0,0 +1,38 @@
+module.exports = {
+	header: {
+		fontSize: 16,
+		bold: true,
+		alignment: 'center',
+		margin: [0, 100, 0, 70],
+	},
+	bold: {
+		bold: true,
+	},
+	subheader: {
+		fontSize: 17,
+	},
+	superMargin: {
+		margin: [35, 0, 35, 0],
+	},
+
+	marginTop: {
+		margin: [35, 20],
+	},
+	marginRight: {
+		margin: [35, 3],
+	},
+	marginFirma: {
+		margin: [35, 140, 0, 40],
+	},
+	font13: {
+		fontSize: 13,
+	},
+	font12: {
+		fontSize: 12,
+	},
+	parrafo: {
+		fontSize: 12,
+		lineHeight: 1.3,
+		alignment: 'justify',
+	},
+};
